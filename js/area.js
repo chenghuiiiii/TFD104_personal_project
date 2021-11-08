d5291c0b81d4a1ff32977b10debe8049c0918053
chenghuiiiii <hou0820emily@gmail.com>
2021-11-08 22:40:19 +0800

    fixing!

--- js/area.js
+++ js/area.js
@@ -3,10 +3,11 @@
   for(let i = 0; i < btn.length; i++){
     btn[i].addEventListener('click', function(){
       let artc = document.getElementsByTagName('article');
-      if(artc[i].getAttribute("class") == "-on"){
-        artc[i].classList.remove("-on");
-      }else{
+      // console.log(artc);
+      if(artc[i].getAttribute("class") != "-on"){
         artc[i].classList.add("-on");
+      }else{
+        artc[i].classList.remove("-on");
       }
     })
   }
